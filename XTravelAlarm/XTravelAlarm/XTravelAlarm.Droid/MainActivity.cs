0ecc576289df894d95fa679bbabc2ca19c6d9722
Dell\Krzysiek <skrzychu7@gmail.com>
2017-03-06 17:17:50 +0100

    Adding Xamarin.Forms.Maps

--- XTravelAlarm/XTravelAlarm/XTravelAlarm.Droid/MainActivity.cs
+++ XTravelAlarm/XTravelAlarm/XTravelAlarm.Droid/MainActivity.cs
@@ -1,17 +1,12 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
 using Prism.Unity;
 using Microsoft.Practices.Unity;
 
 namespace XTravelAlarm.Droid
 {
-    [Activity(Label = "XTravelAlarm", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "XTravelAlarm", Icon = "@drawable/ic_launcher", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
@@ -20,8 +15,10 @@ namespace XTravelAlarm.Droid
             ToolbarResource = Resource.Layout.toolbar;
 
             base.OnCreate(bundle);
+            
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
+            Xamarin.FormsMaps.Init(this, bundle);
             LoadApplication(new App(new AndroidInitializer()));
         }
     }
--- XTravelAlarm/XTravelAlarm/XTravelAlarm.iOS/AppDelegate.cs
+++ XTravelAlarm/XTravelAlarm/XTravelAlarm.iOS/AppDelegate.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-using Foundation;
+﻿using Foundation;
 using UIKit;
 using Prism.Unity;
 using Microsoft.Practices.Unity;
@@ -25,6 +21,7 @@ namespace XTravelAlarm.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
+            Xamarin.FormsMaps.Init();
             LoadApplication(new App(new iOSInitializer()));
 
             return base.FinishedLaunching(app, options);
