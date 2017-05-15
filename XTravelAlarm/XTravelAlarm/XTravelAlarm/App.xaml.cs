26469ca39c5cd4d3646928bec39e12bd34756cf6
Dell\Krzysiek <skrzychu7@gmail.com>
2017-05-15 18:38:25 +0200

    Add lifecycle application.

--- XTravelAlarm/XTravelAlarm/XTravelAlarm/App.xaml.cs
+++ XTravelAlarm/XTravelAlarm/XTravelAlarm/App.xaml.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using Microsoft.Practices.Unity;
+using Plugin.Geolocator;
 using Prism.Unity;
 using Xamarin.Forms;
 using XTravelAlarm.Adapters.Features;
 using XTravelAlarm.Features;
 using XTravelAlarm.Features.AlarmRinging;
 using XTravelAlarm.Features.AlarmRinging.Storage;
-using XTravelAlarm.Models;
 using XTravelAlarm.Services;
 using XTravelAlarm.ViewModels;
 using XTravelAlarm.Views;
@@ -47,6 +48,37 @@ namespace XTravelAlarm
 
             Container.RegisterType<IMainPageFeatures, MainPageFeaturesFacade>();
             Container.RegisterType<IAlarmPageFeatures, AlarmPageFeaturesFacade>();
+        }
+
+        protected override async void OnStart()
+        {
+            base.OnStart();
+            if (!CrossGeolocator.Current.IsListening)
+            {
+                await CrossGeolocator.Current.StartListeningAsync(minTime: 1000, minDistance: 100);
+                Debug.WriteLine("OnStart");
+            }
+        }
+
+
+        protected override async void OnResume()
+        {
+            base.OnResume();
+            if (!CrossGeolocator.Current.IsListening)
+            {
+                await CrossGeolocator.Current.StartListeningAsync(minTime: 1000, minDistance: 100);
+                Debug.WriteLine("OnResume");
+            }
+        }
+
+        protected override async void OnSleep()
+        {
+            base.OnSleep();
+            if (CrossGeolocator.Current.IsListening)
+            {
+                await CrossGeolocator.Current.StopListeningAsync();
+                Debug.WriteLine("OnSleep");
+            }
         }
     }
 }
